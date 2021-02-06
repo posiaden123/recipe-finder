a7a715ac50aeae9f750905186c7a596c4b587d96
posiaden123 <dewest88@gmail.com>
2021-02-05 22:55:40 -0500

    get ingredients for basic pages, set up beginning of utilities

--- finder.py
+++ finder.py
@@ -1,15 +1,20 @@
 import requests
 import sys
+import utility
+import re
 from bs4 import BeautifulSoup
 
 url = input("Paste the url of the page that contains the recipe:")
 try:
     page = requests.get(url)
-except requests.exceptions.HTTPError as e:
-    print("Whoops! The page you were trying to reach encountered an error. Please try again later, or input a new page")
-    print("Status Code", e)
+except requests.exceptions.RequestException as e:
+    print("Whoops! The page you were trying to reach encountered an error.\nPlease try again later, or run the program again to input a new page.")
+    print("Status Code:", e)
     sys.exit()
-soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page.content, 'html.parser')
+ingredients = soup.find_all('div', {'class': re.compile('ingredients')})
+
+
 
 
 
---
+++ utility.py
@@ -0,0 +1,6 @@
+def print_ingredients(ing):
+    for i in ing:
+        print(i.text)
+def get_ingredient_list(site: str):
+    if str == 'all':
+
