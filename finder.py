78d2369978d47e24436103af2b635100623c6bd5
posiaden123 <dewest88@gmail.com>
2021-02-06 00:50:11 -0500

    finished specific parsers for all-recipe and foodnetwork, general parser for other websites

--- finder.py
+++ finder.py
@@ -4,6 +4,7 @@ import utility
 import re
 from bs4 import BeautifulSoup
 
+print("Due to the way some pages set up their website, some directions might get printed multiple times!")
 url = input("Paste the url of the page that contains the recipe:")
 try:
     page = requests.get(url)
@@ -15,15 +16,26 @@ except requests.exceptions.RequestException as e:
 soup = BeautifulSoup(page.content, 'html.parser')
 if 'allrecipes' in str(page.url):
     ingredients = utility.get_ingredients('all', soup)
-    directions =
-elif 'foodnetwork' in str(page.url):
-    a = 0
-else:
-    ingredients = soup.find_all('div', {'class': re.compile('ingredients')})
-    directions = soup.find_all('div', {'class': re.compile("instruction")})
+    directions = utility.get_directions('all', soup)
+    print("Ingredients:\n")
+    utility.print_ingredients(ingredients)
+    utility.print_allrec_directions(directions)
+    print('\n\nThere you go! I hope you enjoyed my recipe finder!')
+    sys.exit()
+if 'foodnetwork' in str(page.url):
+    ingredients = utility.get_ingredients('food', soup)
+    directions = utility.get_directions('food', soup)
+    utility.print_food_ingredients(ingredients)
+    print('\n')
+    utility.print_food_directions(directions)
+    print('\n\nThere you go! I hope you enjoyed my recipe finder!')
+    sys.exit()
 
-
-print("There you go! I hope you enjoyed my scraper!")
+ingredients = soup.find_all('div', {'class': re.compile('ingredients')})
+directions = soup.find_all('div', {'class': re.compile("instruction")})
+utility.print_ingredients(ingredients)
+utility.print_directions(directions)
+print('\n\nThere you go! I hope you enjoyed my recipe finder!')
 
 
 
--- utility.py
+++ utility.py
@@ -5,6 +5,34 @@ def print_ingredients(ing):
     for i in ing:
         print(i.get_text().strip())
 
+def print_directions(dir):
+    for i in dir:
+        print(i.text)
+
 def get_ingredients(web: str, soup: BeautifulSoup):
     if web == 'all':
         return soup.find_all('span', {'class': re.compile('ingredients')})
+    elif web == 'food':
+        return  soup.find_all('span', {'class': re.compile('o-Ingredients__a-Ingredient')})
+def print_food_ingredients(ing):
+    print("Ingredients:\n")
+    for i in ing:
+        print(i.get_text().strip())
+
+def print_allrec_directions(dir):
+    print("Directions:\n")
+    for i in dir:
+        print(i.get_text().strip())
+
+def get_directions(web:str, soup: BeautifulSoup):
+    if web == 'all':
+        return soup.find_all('div', {'class' : 'paragraph'})
+    if web == 'food':
+        return soup.find_all('li', {'class': 'o-Method__m-Step'})
+def print_food_directions(dir):
+    print("Directions:\n")
+    for i in dir:
+        print(i.get_text().strip())
+
+
+
